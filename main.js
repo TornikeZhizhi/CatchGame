528f1ebc7c8dea4db60a2b47405685cfafe994dc
Tornike Zhizhiashvili <Tzhizhiashvili@ia.local>
2019-07-23 11:06:24 +0400

    da

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 
 
 var catchBox = document.getElementById('catchBox');
+var svg = document.querySelector("#catchBox svg")
 var fruit = document.getElementById('fruit');
+
 
 
 
@@ -9,6 +11,7 @@ var fruit = document.getElementById('fruit');
 var windowWidht = window.innerWidth;
 var windowHeight = window.innerHeight;
 
+svg.style.height = window.innerHeight / 2.5
 
 var catchBoxWidth = catchBox.clientWidth
 var catchBoxHeight = catchBox.clientHeight
@@ -17,28 +20,38 @@ var offsetTop = catchBox.offsetTop
 
 
 var moveDown = 0;
-var LeftPoz = Math.floor(Math.random() * 95) + '%';
+var LeftPoz = Math.floor(Math.random() * 95);
 fruit.style.left = LeftPoz + '%';
 
 
 
 setInterval(function(){
 
+
 	if (moveDown > offsetTop) {
+		if (LeftPoz == y || LeftPoz == y + 1 
 
-
+			|| LeftPoz == y + 2 || LeftPoz == y + 3
+			|| LeftPoz == y + 4 || LeftPoz == y + 5
+			|| LeftPoz == y - 1 || LeftPoz == y - 2
+			|| LeftPoz == y - 3 || LeftPoz == y - 4
+			|| LeftPoz == y - 5
+			) {
+			
+			
+		
+		}
 	}
-	if (Number(LeftPoz) == y ) {
-		console.log('aeee')
-	}
 
+	
 
+	
 	
 
 
 
 	fruit.style.top = moveDown + 'px';
-	fruit.style.left = LeftPoz ;
+	fruit.style.left = LeftPoz + '%';
 	moveDown += 10;
 
 
@@ -52,7 +65,7 @@ window.addEventListener('mousemove',function(event){
 	var x = Math.floor(event.clientX / windowWidht * 100);
 	y =  Math.floor(event.clientX / windowWidht * 100);
 
-	
+	console.log()
 	catchBox.style.left = x + '%';
 
 
